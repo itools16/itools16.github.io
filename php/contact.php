4f45a300e9baabe11aa6cb6f74ab79992e1b94f8
itools16 <102628013+itools16@users.noreply.github.com>
2022-06-15 21:58:07 +0100

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -1,10 +1,23 @@
 <? php
      if (isset($_POST['cf-mail'])){
       $name=_POST['name'];
-      $Subject=_POST['cf-subject']
+      $Subject=_POST['subject'];
+      $mailFrom=_POST['mail'];
+      $message=_POST['message'];
+         
+
+$mailto="idreesmayowa@gmail.com";
+$headers="From: ".$mailFrom;
+$txt="you have received an email from".$name.".\n\n".$message;
+ 
+mail($mailto, $message,$txt,$headers);
+header("Location: contact.php?mailsend");
 
 
+}
 
+
+  
 
 
 
